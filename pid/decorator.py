d4f660b01ac17d0723380d2ee06d47867e7957de
trbs <trbs@trbs.net>
2014-05-12 14:52:31 +0200

    pid/decorator.py

---
+++ pid/decorator.py
@@ -0,0 +1,14 @@
+import types
+from functools import wraps
+from . import PidFile
+
+def pidfile(*pid_args, **pid_kwargs):
+    if len(pid_args) > 0:
+        assert not isinstance(pid_args[0], types.FunctionType), "pidfile decorator must be called with perentisies, like: @pidfile()"
+    def wrapper(func):
+        @wraps(func)
+        def decorator(*func_args, **func_kwargs):
+            with PidFile(*pid_args, **pid_kwargs):
+                return func(*func_args, **func_kwargs)
+        return decorator
+    return wrapper
