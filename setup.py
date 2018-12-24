03f88d7331aaccbf9fb152b28732a91b38cd52db
Xorboo <xorboo@gmail.com>
2018-12-24 14:19:07 +0100

    Remove 2.6 support
    Module idna does not support python2.6 now (required by coverage module)

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
