d7858c4a3e0ccff11d084a2b87dde845e8cd4356
Barend Scholtus <barend.scholtus@gmail.com>
2020-07-18 00:36:36 +0700

    Use APPDATA and HOME locations on Windows

--- pid/utils.py
+++ pid/utils.py
@@ -15,16 +15,19 @@ def effective_access(*args, **kwargs):
 
 def determine_pid_directory():
     if sys.platform == "win32":
-        return tempfile.gettempdir()
+        if 'APPDATA' in os.environ:
+            paths = [os.environ['APPDATA']]
+        else:
+            paths = [os.path.expanduser('~\\AppData\\Roaming')]
+    else:
+        uid = os.geteuid() if hasattr(os, "geteuid") else os.getuid()  # pylint: disable=no-member
 
-    uid = os.geteuid() if hasattr(os, "geteuid") else os.getuid()
-
-    paths = [
-        "/run/user/%s/" % uid,
-        "/var/run/user/%s/" % uid,
-        "/run/",
-        "/var/run/",
-    ]
+        paths = [
+            "/run/user/%s/" % uid,
+            "/var/run/user/%s/" % uid,
+            "/run/",
+            "/var/run/",
+        ]
 
     for path in paths:
         if effective_access(os.path.realpath(path), os.W_OK | os.X_OK):
