3952ea5d83b4b6bcf036a3296611acfdd98238ad
Jair Henrique <jair.henrique@gmail.com>
2022-11-03 11:56:20 -0300

    move ci to gh-actions

--- setup.py
+++ setup.py
@@ -41,14 +41,12 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
     keywords='pid pidfile context manager decorator',
@@ -58,7 +56,6 @@ setup(
     ],
     extras_require={
         'tests': [
-            'mock ; python_version < "3"',
             'pytest',
         ]
     },
